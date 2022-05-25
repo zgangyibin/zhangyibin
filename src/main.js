ff185de45f6b92c91be7873a0cd68da8783beb40
zgangyibin <3112891950@qq.com>
2022-05-25 11:55:31 +0800

    shop

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
+// 引入第三方包里的css文件
+import 'bootstrap/dist/css/bootstrap.css'
+axios.defaults.baseURL = "https://www.escook.cn"
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
